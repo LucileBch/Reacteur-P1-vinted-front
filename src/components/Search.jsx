888a65dc1169cd1002eab0df683b97e0c97190d4
LucileBch <l.bouchire@gmail.com>
2024-03-27 14:21:27 +0100

    Adding sort and range input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,16 +3,29 @@ const Search = () => {
   // Listen search changes
   const handleSearch = (event) => {
     const value = event.target.value;
-    console.log(value);
   };
 
+  // Search input
+  // Sort input
+  // Range input
   return (
     <div>
-      <input
-        type="tex"
-        placeholder="Recherche des articles"
-        onChange={handleSearch}
-      />
+      <div>
+        <input
+          type="tex"
+          placeholder="Recherche des articles"
+          onChange={handleSearch}
+        />
+      </div>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <p>Trier par prix</p>
+        <label>
+          <input type="checkbox" />
+        </label>
+        <p>Prix entre : </p>
+        <input type="range" />
+      </div>
     </div>
   );
 };
