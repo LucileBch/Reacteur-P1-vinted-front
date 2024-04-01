d8b3abdbbefd08a6ab0e15f7bf43914b1a5be94c
LucileBch <l.bouchire@gmail.com>
2024-04-01 21:29:49 +0200

    Styling Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,26 @@
 // ---------- ERROR Page ----------
-// Import packages
+// Packages Imports
 import { Link } from "react-router-dom";
+
+// MUI Imports
+import Container from "@mui/material/Container";
+
+// Styles Imports
+import "../styles/Error.css";
 
 const Error = () => {
   return (
-    <div>
-      <p>ERROR 404</p>
-      <Link to="/"> Retour à la page d'accueil</Link>
-    </div>
+    <main className="main__error">
+      <Container maxWidth="lg">
+        <div className="error__display">
+          <p>404 : PAGE NOT FOUND</p>
+          <Link to="/" className="error__link">
+            {" "}
+            Retour à la page d'accueil
+          </Link>
+        </div>
+      </Container>
+    </main>
   );
 };
 
